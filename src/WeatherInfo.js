4e51e223cdbaecf96fdf2bce26118aa770937bb4
Stajgar <73831696+Stajgar@users.noreply.github.com>
2021-01-02 18:11:07 +0100

    Added temperature conversion to display true temperature in celsius and fahrenheit depending on chosen city

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
     return (
@@ -20,13 +21,7 @@ export default function WeatherInfo(props) {
               <WeatherIcon code={props.data.icon} />
               </div>
               <div className="float-left">
-                <span className="temperature">{props.data.temperature}</span>
-                <span className="units">
-                  <a href="/" className="active">
-                    °C
-                  </a>{" "}
-                  |<a href="/">°F</a>
-                </span>
+              <WeatherTemperature celsius={props.data.temperature} />
               </div>
             </div>
           </div>
